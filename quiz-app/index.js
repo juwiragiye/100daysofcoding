c9ea0cf03beea72e5ab9c8ac09682c330e157a25
juwiragiye <uwiragiyejanvi@gmail.com>
2021-12-05 22:26:55 -0500

    Update README.md, apply css fundamentals, react  basics

--- quiz-app/index.js
+++ quiz-app/index.js
@@ -1,77 +1,100 @@
-const questions = [
+const quizData = [
   {
-    id: 1,
-    question: 'Which planet in our solar system is closest to the sun?',
-    choices: ['Mars', 'Mercury', 'Jupiter'],
-    answer: 'Mercury',
+    question: 'What is the most used programming language in 2019?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'd',
   },
   {
-    id: 2,
-    question:
-      'Which actor played Richard III in the 1995 British film drama of the same title?',
-    choices: ['Ian McKellen', 'Partrick Stewart', 'Elijah Wood'],
-    answer: 'Ian McKellen',
+    question: 'Who is the President of US?',
+    a: 'Florin Pop',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Mihai Andrei',
+    correct: 'b',
   },
   {
-    id: 3,
-    question:
-      'Which Marvel superhero, also known as Steve Rogers, made his first appearance in March 1941?',
-    choices: ['Superman', 'Black Panther', 'Captain America'],
-    answer: 'Captain America',
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'Jason Object Notation',
+    d: 'Helicopters Terminals Motorboats Lamborginis',
+    correct: 'a',
   },
   {
-    id: 4,
-    question: 'Which English football club play at Roots Hall?',
-    choices: ['Liverpool', 'Southend United', 'Wolverhampton'],
-    answer: 'Southend United',
+    question: 'What year was JavaScript launched?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: 'none of the above',
+    correct: 'b',
   },
 ];
 
-const textOne = document.getElementById('t-one');
-const textTwo = document.getElementById('t-two');
-const textThree = document.getElementById('t-three');
-const question = document.querySelector('.question-text');
-const questionChoices = document.querySelector('.question-choices');
-const submitButton = document.querySelector('button');
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
 
-let questionNumber = 0;
-let answer = undefined;
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
 
 function loadQuiz() {
-  const currentQuestion = questions[questionNumber];
-  let currentAnswer = 0;
-  question.textContent = currentQuestion.question;
-  textOne.textContent = currentQuestion.choices[currentAnswer];
-  textTwo.textContent = currentQuestion.choices[currentAnswer + 1];
-  textThree.textContent = currentQuestion.choices[currentAnswer + 2];
-  submitButton.addEventListener('click', () => {
-    if (questionNumber < questions.length) {
-      loadQuiz();
-      //   return;
-      //   questionNumber;
-    } else {
-      alert('You finished! Do this again later.👋');
-    }
-    const selectedId = getSelected();
-    console.log(selectedId);
-    const chosenAnswer = document.querySelectorAll('answer')[selectedId];
-    console.log(chosenAnswer);
+  deselectAnswers();
 
-    // return;
-    // questionNumber++;
+  const currentQuizData = quizData[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+  let answer = undefined;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
   });
 }
 
-const getSelected = () => {
-  const answers = document.querySelectorAll('.answer');
-  for (i = 0; i < answers.length; i++) {
-    if (answers[i].checked) {
-      answer = i;
+submitBtn.addEventListener('click', () => {
+  // check to see the answer
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+              <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+              
+              <button onclick="location.reload()">Reload</button>
+          `;
     }
   }
-  if (answer) {
-    return answer;
-  }
-};
-
-loadQuiz();
+});
---
+++ react-basics/index.js
@@ -0,0 +1,8 @@
+ReactDOM.render(
+  <ul>
+    <li>Item One</li>
+    <li>Item Two</li>
+    <li>Item Three</li>
+  </ul>,
+  document.getElementById('root')
+);
