4e071ac920bdf38a90098d9003f52ce52e977ce0
Janvier Uwiragiye <uwiragiyejanvi@gmail.com>
2022-04-01 21:56:47 -0400

    day project

---
+++ day1/conversion.py
@@ -0,0 +1,9 @@
+def from_miles_km(miles: float) -> float:
+    return  round(miles * 1.609344, 2)
+
+def print_kms(kms: float, miles: float) -> str:
+    return f"Kilometers {kms} are in {miles} miles"
+
+
+
+
--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from conversion import from_miles_km, print_kms
 
 
 def setMessage(message):
@@ -11,21 +12,29 @@ root.minsize(width=800, height=600)
 
 
 
-label = ttk.Label(text="Hello World", font=("Arial", 24, "bold"))
+label = ttk.Label(text="Miles to Km Converter", font=("Arial", 24, "bold"))
 label.pack()
-input = ttk.Entry()
-input.pack()
 
-def click():
-    label["text"] = setMessage()
+label2 = ttk.Label(text="Miles to Km Converter", font=("Arial", 18, "bold"))
+label2.pack(side="left")
 
-button = ttk.Button(text="Click!", width=10, command=click)
-button.pack()
+inputMiles = ttk.Entry( width=20)
+inputMiles.pack(side="left")
 
 
 
 
+miles = inputMiles.get()
 
+
+label3 = ttk.Label(text=f"Kms are:  ", font=("Arial", 18, "bold"))
+label3.pack(side="bottom")
+def convert():
+    label3['text'] = print_kms(from_miles_km(float(inputMiles.get())), float(inputMiles.get()))
+
+
+
+ttk.Button(text="Convert", command=convert).pack(side="left")
 
 if __name__ == "__main__":
     root.mainloop()
