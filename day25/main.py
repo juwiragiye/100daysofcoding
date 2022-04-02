2d054d90486268a3215e7442233c8885dbddc0ef
Janvier Uwiragiye <uwiragiyejanvi@gmail.com>
2022-04-02 12:47:53 -0400

    pandas

---
+++ day25/main.py
@@ -0,0 +1,10 @@
+import csv
+import pandas as pd
+import numpy as np
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("./weather_data.csv")
+    df.temp = df.temp * 1.8 + 32 
+    print(df.temp)
+    
