999bb9fe324b8c52d0fd52c805b844cabd86d2bf
Janvier Uwiragiye <uwiragiyejanvi@gmail.com>
2022-04-07 23:12:13 -0400

    Day one done

--- bmi-calculator-flutter/ios/Flutter/flutter_export_environment.sh
+++ bmi-calculator-flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\uwira\code\github\100daysofcoding\bmi-calculator-flutter"
+export "FLUTTER_ROOT=/Users/janvier/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/janvier/Developer/100daysofcode/bmi-calculator-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
